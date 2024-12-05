fc426fd7c3e4a0905a9fa7f7409b5d1fb9f5e2f1
zunnukhan <hajira_khan_786@yahoo.com>
2024-12-05 21:37:29 +0530

    Create 2activity1.js

---
+++ 2activity1.js
@@ -0,0 +1,14 @@
+<html>
+  <body>
+    <p id="m1"></p>
+    <button onclick="mfunction(7,4)">JS function</button>
+    <script>
+      function mfunction(x,y){
+    let z=x-y;
+        document.getElementById("m1").innerHTML=z;
+  
+      }
+      
+    </script>
+  </body>
+</html>
