38e62c7e5b3e70a1b9fe55ee37d606e60937896b
zunnukhan <hajira_khan_786@yahoo.com>
2024-12-08 19:38:46 +0530

    Create js data types.js

---
+++ js data types.js
@@ -0,0 +1,23 @@
+<html>
+  <body>
+    <h1>Java Script data types</h1>
+    <script>
+     
+       var a = 12;
+      var b = 12;
+      var c = 10;
+      var d = null;
+      var e = // undefined variable 
+          document.write("a and b are equal:"+(a==b))
+      document.write("<br>")
+      document.write("a and c are equal:"+(a==b))
+      document.write("<br>")
+      document.write("c and b are equal:"+(a==b))
+      document.write("<br>")
+      document.write("the value of e is"+ e)
+      document.write("<br>")
+      document.write("the value of e is"+ d)
+    </script>
+  </body>
+</html>
+     
