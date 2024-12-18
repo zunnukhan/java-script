8e123afda2447517e6abb42f1747bb54af7dd48b
zunnukhan <hajira_khan_786@yahoo.com>
2024-12-18 20:12:02 +0530

    Create arrow function.js

---
+++ arrow function.js
@@ -0,0 +1,9 @@
+<html>
+  <body>
+    <p id="result"></p>
+    <script>
+      add=(a,b) => a+b;
+      document.getElementById("result").innerHTML=add (4,6);
+    </script>
+    </body>
+      </html>  
