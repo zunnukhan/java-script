17ab6841b5c3a1b98997b087195511c5e262cbf2
zunnukhan <hajira_khan_786@yahoo.com>
2024-12-21 12:02:30 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,12 @@
+<html>
+  <body>
+    <p id="demo"></p>
+    <script>
+      var friend= {
+        friendname:"I ",
+        age:12 ,
+      };
+      document.getElementById("demo").innerHTML=friend.friendname + "am "+ friend.age+"year old.";
+      </script>
+       </body> 
+      </html>
