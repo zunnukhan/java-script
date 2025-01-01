4b57c5b330a771c7cfec5e71bda5633587f6943e
zunnukhan <hajira_khan_786@yahoo.com>
2025-01-01 15:17:48 +0530

    Create perimeter calculator acp.js

---
+++ perimeter calculator acp.js
@@ -0,0 +1,19 @@
+var choice=prompt("Welcome to perimeter calculator.\n Please enter your choice.\n1 Perimeter of rectangle.\n2 Perimeter of triangle.\n3 Perimeter of square);
+  if (choice=="1") {
+  var l=prompt("enter the Length")
+   var b=prompt("enter the breath")
+   var result=Number(l)*Number(b)*Number(l)*Number(b)
+   alert("the perimeteris"+ result)
+}  
+if (choice=="2") {
+  var s1=prompt("enter the side1")
+   var s2=prompt("enter the side2")
+    var s3=prompt("enter the side3")
+   var result=Number(s1)+Number(s2)+Number(s3)
+   alert("the perimeteris"+ result)
+}                 
+if (choice=="3") {
+  var side=prompt("enter the sides")
+   var result=Number(side)*Number(side)*Number(side)*Number(side)
+   alert("the perimeteris"+ result)
+}                 
